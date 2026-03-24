28150871cba0e8e2f52762b37e82acad228fde76
Cricket08 <122630388+Cricket08@users.noreply.github.com>
2026-03-24 11:58:16 -0400

    almost done interpolation

--- src/main/java/frc/robot/commands/Turret/ShootAtTarget.java
+++ src/main/java/frc/robot/commands/Turret/ShootAtTarget.java
@@ -8,28 +8,32 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Turret;
-import swervelib.SwerveDrive;
-import frc.util.PolynomialRegression;
-import frc.util.*;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 /** An example command that uses an example subsystem. */
 public class ShootAtTarget extends Command {
   @SuppressWarnings("PMD.UnusedPrivateField")
 
-  private static final List<Point> data = Arrays.asList(
-      new Point(1.0, 2500),
-      new Point(2,1)
-  );
+  private static final InterpolatingDoubleTreeMap table = new InterpolatingDoubleTreeMap();
+  
+    static {
+      table.put(1.0,1.0);
+      table.put(2.0,4.0);
+      table.put(3.0,9.0);
+  }
 
-  public static final PolynomialRegression distanceRegression =
-      new PolynomialRegression(data, 1);
-
-  private final SwerveDrive drivebase;
+  private final SwerveSubsystem drivebase;
   private final Turret turret;
 
 
@@ -38,7 +42,7 @@ public class ShootAtTarget extends Command {
    *
    * @param subsystem The subsystem used by this command.
    */
-   public ShootAtTarget(Turret turret, SwerveDrive drivebase) {
+   public ShootAtTarget(Turret turret, SwerveSubsystem drivebase) {
     this.turret = turret;
     this.drivebase = drivebase;
 
@@ -52,13 +56,17 @@ public class ShootAtTarget extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    Pose2d hub = null;
     // m_turret.
    if (DriverStation.getAlliance().get() == Alliance.Red){
-      
+      hub = new Pose2d(39.05, 13.19, new Rotation2d());
    }
     if (DriverStation.getAlliance().get() == Alliance.Blue){
-    
+      hub = new Pose2d(15.13, 13.19, new Rotation2d());
    }
+   Transform2d transformToHub = drivebase.getSwerveDrive().getPose().minus(hub);
+   double distanceToHub = Math.sqrt(Math.pow(transformToHub.getX(), 2)+Math.pow(transformToHub.getY(), 2));
+   double predictedAngle = table.get(distanceToHub);
   }
 
   // Called once the command ends or is interrupted.
