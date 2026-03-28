e528aa4797cc54641f0ad3930e785de97bfe4226
ALD Lions <aldrobotics9263@gmail.com>
2026-03-27 21:54:18 -0400

    Hood should be good to test now

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -5,13 +5,14 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import java.util.List;
-
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import static frc.robot.Constants.HoodConstants.*;
+// import static frc.robot.Constants.HoodConstants.*;
 
 import java.util.function.DoubleSupplier;
 
@@ -20,21 +21,28 @@ public class Hood extends SubsystemBase {
     private final SparkClosedLoopController controller = motor.getClosedLoopController();
     private final SparkMaxConfig config = new SparkMaxConfig();
 
-    private final double[] rots = {0.0, 0.0, 0.0};
+    private final double[] rots = {0.0, -11, -26};
     private int i = 0;
 
     public Hood() {
-        config.closedLoop.pid(0.0, 0.0, 0.0);
-        config.closedLoop.outputRange(0, 1); // PLACEHOLDER
+        config.closedLoop.pid(0.005, 0, 0);
+        config.closedLoop.outputRange(-1, 1); // PLACEHOLDER
+
+        motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+
+
+        Shuffleboard.getTab(getName()).addDouble("Hood angle", () -> motor.getEncoder().getPosition());
+        Shuffleboard.getTab(getName()).addDouble("Hood setpoint", () -> getSetpoint());
+
+
         // config.encoder.positionConversionFactor(360);
     }
 
     public Command setHoodAngle(double angle) {
         return runOnce(
             () -> {
-                double truAngle = (angle / 360) / (2000 / 7);
-                if (truAngle >= 0.0 && truAngle <= 71.0)
-                controller.setSetpoint(truAngle, ControlType.kPosition);
+                // if (angle >= MINANGLEROT && angle <= MAXANGLEROT) shouldnt be necessary with only three setpoints
+                controller.setSetpoint(angle, ControlType.kPosition);
                 
             }
         );
@@ -42,19 +50,19 @@ public class Hood extends SubsystemBase {
 
     @Override
     public void periodic(){
-        SmartDashboard.putNumber("Hood angle", motor.getEncoder().getPosition());
-        SmartDashboard.putNumber("Hood setpoint", getSetpoint());
+        // SmartDashboard.putNumber("Hood angle", motor.getEncoder().getPosition());
+        // SmartDashboard.putNumber("Hood setpoint", getSetpoint());
         setHoodAngle(rots[i]);
     }
 
-    // public Command setHoodAngle(DoubleSupplier radianSupplier){
-    //     return defer(() -> {
-    //         return setHoodAngle(radianSupplier.getAsDouble());
-    //     });
-    // }
+    public Command setHoodAngle(DoubleSupplier radianSupplier){
+        return defer(() -> {
+            return setHoodAngle(radianSupplier.getAsDouble());
+        });
+    }
 
     public double getSetpoint(){
-        return controller.getSetpoint() * 360;
+        return controller.getSetpoint();
     }
 
     public Command iterateRot() { 
